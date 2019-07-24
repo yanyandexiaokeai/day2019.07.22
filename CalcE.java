64e641d0302435d20d5d4a0be1eef78f7677e862
yanyandexiaokeai <47706476+yanyandexiaokeai@users.noreply.github.com>
2019-07-24 17:06:59 +0800

    Update CalcE.java

--- CalcE.java
+++ CalcE.java
@@ -17,6 +17,13 @@ public  class CalcE{
 		}
 		return r;
 	}
+	/**
+     * 利用公式
+     * e = 1/0! + 1/1! + 1/2! + 1/3! + 1/4! + ...
+     * 计算自然对数的底 e
+     * @param n 计算多少项
+     * @return
+     */
 	public static double calcE(int n){
 	     double e=0;
 		for(int i=0;i<=n;i++){
