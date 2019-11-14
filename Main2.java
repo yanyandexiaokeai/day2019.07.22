8e7d01d4010264db8e7a1b50e1eb831f06c18390
小六 <1203258449@qq.com>
2019-11-14 15:31:10 +0800

    求最小公倍数

---
+++ Main2.java
@@ -0,0 +1,16 @@
+//求最小公倍数
+import java.util.Scanner;
+public class Main2 {
+    public static int lcm(int m, int n) {
+        return (m * n) / gcd(m, n);
+    }
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(lcm(a, b));
+    } }
